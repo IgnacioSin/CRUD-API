a27a8562fee3a9f660f9981c093602a641f89472
Juan Ignacio Sin <ignaciosin02@gmail.com>
2026-08-10 04:02:59 -0300

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from fastapi import FastAPI
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi import Request
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title: str | None = None
+
 
 list_of_tasks = [
     { "id": 1, "title": "Go Gym", "done": False },
     { "id": 2, "title": "Buy Groceries", "done": False },
     { "id": 3, "title": "Walk the Dog", "done": False }
 ]
+
+next_id = 4
 
 app = FastAPI()
 
@@ -40,3 +47,21 @@ def get_task(id: int):
         if task["id"] == id:
             return task
     raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
+
+@app.post("/tasks", status_code=201)
+def create_task(task: TaskCreate):
+
+    if not task.title or not task.title.strip():
+            raise HTTPException(status_code=400, detail="Title is missing")
+
+    global next_id
+    new_task = {
+        "id": next_id,
+        "title": task.title,
+        "done": False
+    }
+    next_id += 1
+
+    list_of_tasks.append(new_task)
+    return new_task
